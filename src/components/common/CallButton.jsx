d7930e4c27e6f809af49f486fe3acec4d9c9191b
Solomon Swager <zitiswager@gmail.com>
2025-10-03 20:34:53 -0400

    updated call button

--- src/components/common/CallButton.jsx
+++ src/components/common/CallButton.jsx
@@ -1,7 +1,7 @@
 export default function CallButton () {
     return (
         <a
-            href="tel:7405919124"
+            href="tel:+16145245721"
             className="fixed bottom-6 right-6 z-50 bg-green-600 text-white p-4 rounded-full shadow-lg md:hidden"
             aria-label="Call Grey Techs"
             >
